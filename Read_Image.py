937a9df7fb07443bd8d9c62afeae4b98df8d8659
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2024-08-24 13:38:25 -0300

    Read_Image.py

--- Read_Image.py
+++ Read_Image.py
@@ -5,14 +5,11 @@ import cv2
 #binaria 101010101010101 [40 148 75]
 
 #uma leitura da imagem
-img = cv2.imread("butterfly.jpg")
+
 
 #mostrar a imagem
-#cv2.imshow("Imagem de exibição", img)
-#print(img)
 
-gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-cv2.imshow("Imagem de exibição", gray_img)
-print(gray_img)
-cv2.waitKey(0)
+
+
+
 
